4432b0594833792134694cd0d706ee51bcea9da9
Oleksandr Ostapchuk <shurion.ua@gmail.com>
2022-01-19 16:50:03 +0200

    Spring-part added to project

---
+++ src/main/java/cinema/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,29 @@
+package cinema.dao.impl;
+
+import cinema.dao.AbstractDao;
+import cinema.dao.RoleDao;
+import cinema.exception.DataProcessingException;
+import cinema.model.Role;
+import java.util.Optional;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
+    public RoleDaoImpl(SessionFactory factory) {
+        super(factory, Role.class);
+    }
+
+    @Override
+    public Optional<Role> getByName(String name) {
+        try (Session session = factory.openSession()) {
+            return session
+                    .createQuery("FROM Role WHERE roleName = :name", Role.class)
+                    .setParameter("name", Role.RoleName.valueOf(name))
+                    .uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find role by name: " + name, e);
+        }
+    }
+}
--- src/main/java/cinema/model/ShoppingCart.java
+++ src/main/java/cinema/model/ShoppingCart.java
@@ -3,26 +3,20 @@ package cinema.model;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table (name = "shopping_carts")
+@Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
     private Long id;
     @OneToMany
-    @JoinTable(name = "shoping_carts_tickets",
-            joinColumns = @JoinColumn(name = "order_id"),
-            inverseJoinColumns = @JoinColumn(name = "ticket_id"))
     private List<Ticket> tickets;
-    @OneToOne
     @MapsId
-    @JoinColumn(name = "id")
+    @OneToOne
     private User user;
 
     public Long getId() {
@@ -51,7 +45,9 @@ public class ShoppingCart {
 
     @Override
     public String toString() {
-        return "ShoppingCart{" + "id=" + id + ", tickets="
-                + tickets + ", user=" + user + '}';
+        return "ShoppingCart{"
+                + "id=" + id
+                + ", tickets=" + tickets
+                + ", user=" + user + '}';
     }
 }
